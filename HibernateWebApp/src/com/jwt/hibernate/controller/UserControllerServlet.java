30489d3e02b52b4d6c0c9953f1f53a0f0f5ad00b
yunus <yunus.dursun.turan@gmail.com>
2015-06-29 11:05:05 +0300

    proje1

---
+++ HibernateWebApp/src/com/jwt/hibernate/controller/UserControllerServlet.java
@@ -0,0 +1,37 @@
+package com.jwt.hibernate.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+ 
+import com.jwt.hibernate.dao.UserDAO;
+ 
+public class UserControllerServlet extends HttpServlet {
+     
+    private static final long serialVersionUID = 1L;
+ 
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+ 
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password1");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+        String city = request.getParameter("city");
+ 
+        HttpSession session = request.getSession(true);
+        try {
+            UserDAO userDAO = new UserDAO();
+            userDAO.addUserDetails(userName, password, email, phone, city);
+            response.sendRedirect("Success");
+        } catch (Exception e) {
+ 
+            e.printStackTrace();
+        }
+ 
+    }
+}
---
+++ HibernateWebApp/src/com/jwt/hibernate/dao/UserDAO.java
@@ -0,0 +1,44 @@
+package com.jwt.hibernate.dao;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+ 
+import com.jwt.hibernate.bean.User;
+ 
+public class UserDAO {
+ 
+    public void addUserDetails(String userName, String password, String email,
+            String phone, String city) {
+        try {
+            // 1. configuring hibernate
+            Configuration configuration = new Configuration().configure();
+ 
+            // 2. create sessionfactory
+            SessionFactory sessionFactory = configuration.buildSessionFactory();
+ 
+            // 3. Get Session object
+            Session session = sessionFactory.openSession();
+ 
+            // 4. Starting Transaction
+            Transaction transaction = session.beginTransaction();
+            User user = new User();
+            user.setUserName(userName);
+            user.setPassword1(password);
+            user.setEmail(email);
+            user.setCity(city);
+            user.setPhone(phone);
+            session.save(user);
+            transaction.commit();
+            System.out.println("\n\n Details Added \n");
+ 
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            System.out.println("error");
+        }
+ 
+    }
+ 
+}
